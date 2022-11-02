4fee6c4ab5801f76a4538180d2afa6865daffb08
Adrians <adrianson@gmail.com>
2022-11-02 18:27:22 +0200

    Prototype data extractor

---
+++ SANDataExtractor.py
@@ -0,0 +1,51 @@
+import copy
+import re
+
+class Nom:
+  pass
+
+noms = []
+
+f = open("source.txt", "r")
+for x in f:
+  #print(x)
+  if "article" in x:
+    currentNom = Nom()
+    currentNom.article = x
+  elif "result" in x:
+    currentNom.result = x
+  elif "nominator" in x:
+    currentNom.nominator = x
+  elif "startdate" in x:
+    currentNom.startdate = x
+  elif "WPs" in x:
+    currentNom.WPs = x
+  elif "votes" in x:
+    currentNom.votes = x
+  elif "objectors" in x:
+    currentNom.objectors = x
+  elif "enddate" in x:
+    currentNom.enddate = x
+    noms.append(copy.deepcopy(currentNom))
+f.close()
+
+f = open("result.txt", "a")
+for x in noms:
+    print(x.article.rstrip() + ", " +
+    x.result + ", " +
+    x.nominator + ", " +
+    x.startdate + ", " +
+    x.WPs + ", " +
+    x.votes + ", " +
+    x.objectors + ", " +
+    x.enddate)
+
+    f.write(x.article.rstrip() + ", " +
+    x.result.rstrip() + ", " +
+    x.nominator.rstrip() + ", " +
+    x.startdate.rstrip() + ", " +
+    x.WPs.rstrip() + ", " +
+    x.votes.rstrip() + ", " +
+    x.objectors.rstrip() + ", " +
+    x.enddate.rstrip() + "\n")
+f.close()

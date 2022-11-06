7671069983cac1ab4ad5d4cbadf15c69a3cdc6eb
Adrians <adrianson@gmail.com>
2022-11-06 20:18:36 +0200

    Extract title, result, nominator, dates

--- SANDataExtractor.py
+++ SANDataExtractor.py
@@ -2,19 +2,12 @@ import copy
 import re
 
 class Nom:
-  def __init__(self):
-    self.article = ""
-    self.result = ""
-    self.nominator = ""
-    self.WPs = ""
-    self.votes = ""
-    self.objectors = ""
-    self.enddate = ""
+  pass
 
 noms = []
 patternArticle = "^\[\[Wookieepedia:Comprehensive article nominations/.*\]\]$"
 patternResult = "^:''The following discussion is preserved as an archive of a \[\[Wookieepedia:Comprehensive article nominations\|Comprehensive article nomination\]\] that was '''.*'''."
-patternNominator = "^\*'''Nominated by''': .*$"
+patternNominator = "^\*'''Nominated by''':.*$"
 patternWPs = "^\*'''WookieeProject \(optional\)''':.*$"
 patternVotes = "^====Support====.*$"
 patternObjectors = "^====Object====.*$"
@@ -25,19 +18,27 @@ for x in f:
   #print(x)
   if re.search(patternArticle, x):
     currentNom = Nom()
-    currentNom.article = re.findall(patternArticle, x)[0]
+    currentNom.article = re.sub("(^\[\[Wookieepedia:Comprehensive article nominations/|\]\])", "", x).strip()
   elif re.search(patternResult, x):
-    currentNom.result = re.findall(patternResult, x)[0]
+    currentNom.result = re.sub("(^:''The following discussion is preserved as an archive of a \[\[Wookieepedia:Comprehensive article nominations\|Comprehensive article nomination\]\] that was '''|\.|''')", "", x).strip()
   elif re.search(patternNominator, x):
-    currentNom.nominator = re.findall(patternNominator, x)[0]
+    string = re.sub("^\*'''Nominated by''':[^\[]*", "", x).strip()
+    namePart = re.findall("\[\[User:.*\|", string)[0]
+    name = re.sub("(\[\[User:|\|.*)", "", namePart)
+    datePart = re.findall("\d\d:\d\d, \d+ (?:January|February|March|April|May|June|July|August|September|October|November|December) \d\d\d\d \(UTC\)", string)[0]
+    date = re.sub(" \(UTC\)", "", datePart)
+    currentNom.nominator = name
+    currentNom.startdate = date
   elif re.search(patternWPs, x):
-    currentNom.WPs = re.findall(patternWPs, x)[0]
+    WPlist = re.sub("^\*'''WookieeProject \(optional\)''':", "", x).strip()
+    WPs = re.findall("", WPlist)
+    currentNom.WPs = WPlist
   elif re.search(patternVotes, x):
     currentNom.votes = re.findall(patternVotes, x)[0]
   elif re.search(patternObjectors, x):
     currentNom.objectors = re.findall(patternObjectors, x)[0]
   elif re.search(patternEnddate, x):
-    currentNom.enddate = re.findall(patternEnddate, x)[0]
+    currentNom.enddate = re.sub("(^.*approved\|| \(UTC\)|\}\})", "", x).strip()
     noms.append(copy.deepcopy(currentNom))
 f.close()
 
@@ -47,8 +48,9 @@ for x in noms:
     f.write(x.article.rstrip() + ",\n" +
     x.result.rstrip() + ",\n" +
     x.nominator.rstrip() + ",\n" +
+    x.startdate.rstrip() + ",\n" +
     x.WPs.rstrip() + ",\n" +
     x.votes.rstrip() + ",\n" +
     x.objectors.rstrip() + ",\n" +
-    x.enddate.rstrip() + "\n")
+    x.enddate.rstrip() + ",\n")
 f.close()
